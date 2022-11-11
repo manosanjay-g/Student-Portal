acb82076c0abfd5b2dce6b65a6741c903cf96784
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 18:54:11 +0530

    added navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
+import NavigationBar from "./components/core/NavigationBar.vue";
 </script>
 
 <template>
-  <RouterView />
+  <div id="global" class="flex flex-row text-white min-h-screen">
+    <NavigationBar class="bg-gray-900 lg:basis-1/4" />
+    <RouterView class="bg-gray-800 lg:basis-3/4" />
+  </div>
 </template>
---
+++ client/src/components/core/NavigationBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    id="navigation-bar"
+    class="flex flex-col items-center justify-center text-center"
+  >
+    <div id="user-info">
+      <p class="text-3xl font-semibold mt-4 mb-2">{{ userDetails.name }}</p>
+      <p class="text-slate-400 text-xl">{{ userDetails.course }}</p>
+    </div>
+    <div id="nav-items" class="my-8">
+      <router-link to="/"
+        ><p class="my-4 text-2xl text-medium">Home</p></router-link
+      >
+      <router-link to="/my-info/exam-results/123"
+        ><p class="my-4 text-2xl text-medium">Exam Results</p></router-link
+      >
+      <router-link to="/my-info/my-profile/123"
+        ><p class="my-4 text-2xl text-medium">My Profile</p></router-link
+      >
+      <router-link to="/attendance"
+        ><p class="my-4 text-2xl text-medium">Attendance</p></router-link
+      >
+    </div>
+    <p class="text-xl text-red-600 font-medium cursor-pointer">Log out</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const userDetails = ref({
+  name: "Mano Sanjay",
+  course: "B.E Computer Science and Engineering",
+});
+</script>
