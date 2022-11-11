ad691edbc186397f8354c14646f9e462dec720cb
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 21:03:16 +0530

    finished attendance ui

---
+++ client/src/components/attendance/AttendanceReportCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    id="exam-results-card"
+    class="mx-4 my-2 max-h-90 bg-neutral-900 rounded-lg text-center py-2"
+  >
+    <p class="px-2 py-2 text-2xl font-medium">
+      ATTENDANCE REPORT - EVEN 2021-22
+    </p>
+    <hr />
+    <div class="flex flex-row px-4 m-4">
+      <p class="basis-1/5 text-center">Subject Code</p>
+      <p class="basis-1/5 text-center">Subject Name</p>
+      <p class="basis-1/5 text-center">Present Count</p>
+      <p class="basis-1/5 text-center">Absent Count</p>
+      <p class="basis-1/5 text-center">Total</p>
+    </div>
+    <div
+      v-for="subject in props.subjects"
+      :key="subject._id"
+      class="m-4 flex flex-row rounded-md bg-neutral-800 items-center px-4 py-6"
+    >
+      <p class="basis-1/5 text-center">{{ subject.subject_code }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_name }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_present }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_absent }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_total }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["subjects"]);
+</script>
--- client/src/views/AttendanceView.vue
+++ client/src/views/AttendanceView.vue
@@ -1,5 +1,52 @@
 <template>
-  <div id="attendance-view">
-    <p>Attendance View</p>
+  <div id="attendance-view" class="py-8 px-16">
+    <AttendanceReportCard :subjects="subjectAttendanceReport" />
+    <div class="flex flex-row mx-4 my-8 justify-between">
+      <p class="text-xl font-medium">Total Percentage : 100%</p>
+      <p class="text-xl font-medium">Updated on 29-09-2022</p>
+    </div>
   </div>
 </template>
+
+<script setup>
+import AttendanceReportCard from "../components/attendance/AttendanceReportCard.vue";
+import { ref } from "vue";
+
+const subjectAttendanceReport = ref([
+  {
+    subject_code: "CS19441",
+    subject_name: "Operating Systems",
+    subject_present: "20",
+    subject_absent: "0",
+    subject_total: "20",
+  },
+  {
+    subject_present: "20",
+    subject_code: "CS19442",
+    subject_name: "Software Engineering",
+    subject_absent: "0",
+    subject_total: "20",
+  },
+  {
+    subject_present: "20",
+    subject_code: "CS19443",
+    subject_name: "Database Management Systems",
+    subject_absent: "0",
+    subject_total: "20",
+  },
+  {
+    subject_present: "20",
+    subject_code: "CS19444",
+    subject_name: "Probability and Statistics",
+    subject_absent: "0",
+    subject_total: "20",
+  },
+  {
+    subject_present: "20",
+    subject_code: "CS19445",
+    subject_name: "Soft Skills",
+    subject_absent: "0",
+    subject_total: "20",
+  },
+]);
+</script>
