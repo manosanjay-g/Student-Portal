8d6e328b4726f4fe2a531d21fee72c0f5edb9755
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 23:22:51 +0530

    made navbar sticky

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,8 +4,11 @@ import NavigationBar from "./components/core/NavigationBar.vue";
 </script>
 
 <template>
-  <div id="global" class="flex flex-row text-white min-h-screen">
-    <NavigationBar class="bg-neutral-900 lg:basis-1/4" />
-    <RouterView class="bg-neutral-800 lg:basis-3/4" />
+  <div id="global" class="flex flex-row bg-neutral-900 text-white min-h-screen">
+    <NavigationBar
+      class="bg-neutral-900 lg:basis-1/4 max-h-screen sticky left-0 top-0 z-50"
+    />
+    <RouterView class="bg-neutral-800 lg:basis-3/4 relative" />
   </div>
 </template>
+<style></style>
