fc63627c88880f0e82456800f029b24c443c1761
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 23:22:43 +0530

    added scroll bar

--- client/src/components/home/eventsCard.vue
+++ client/src/components/home/eventsCard.vue
@@ -1,20 +1,22 @@
 <template>
   <div
     id="events-card"
-    class="min-w-auto max-w-lg m-4 max-h-90 bg-neutral-900 rounded-lg text-center py-2"
+    class="min-w-auto max-w-lg m-4 bg-neutral-900 rounded-lg text-center py-2"
   >
     <p class="px-2 py-2 text-2xl font-medium">{{ props.card_name }}</p>
     <hr />
-    <div
-      v-for="event in events"
-      :key="event.event_name"
-      class="m-4 flex flex-row rounded-md bg-neutral-800 items-center px-4 py-6"
-    >
-      <div class="flex flex-col basis-1/4">
-        <p>{{ event.event_date }}</p>
-        <p>{{ event.event_time }}</p>
+    <div class="overflow-auto max-h-80">
+      <div
+        v-for="event in events"
+        :key="event.event_name"
+        class="m-4 flex flex-row rounded-md bg-neutral-800 items-center px-4 py-6"
+      >
+        <div class="flex flex-col basis-1/4">
+          <p>{{ event.event_date }}</p>
+          <p>{{ event.event_time }}</p>
+        </div>
+        <p class="basis-3/4 text-left">{{ event.event_name }}</p>
       </div>
-      <p class="basis-3/4 text-left">{{ event.event_name }}</p>
     </div>
   </div>
 </template>
