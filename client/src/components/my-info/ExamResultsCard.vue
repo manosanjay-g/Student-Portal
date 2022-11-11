780a35a6634e581a5c28b87699ab5ea9e303f1bb
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 21:03:34 +0530

    finished exam results ui

---
+++ client/src/components/my-info/ExamResultsCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    id="exam-results-card"
+    class="mx-4 my-2 max-h-90 bg-neutral-900 rounded-lg text-center py-2"
+  >
+    <p class="px-2 py-2 text-2xl font-medium">
+      END SEMESTER EXAMINATION RESULTS - EVEN 2021-22
+    </p>
+    <hr />
+    <div class="flex flex-row px-4 m-4">
+      <p class="basis-1/5 text-center">Semester</p>
+      <p class="basis-1/5 text-center">Subject Code</p>
+      <p class="basis-1/5 text-center">Subject Name</p>
+      <p class="basis-1/5 text-center">Grade</p>
+      <p class="basis-1/5 text-center">Result</p>
+    </div>
+    <div
+      v-for="subject in props.subjects"
+      :key="subject._id"
+      class="m-4 flex flex-row rounded-md bg-neutral-800 items-center px-4 py-6"
+    >
+      <p class="basis-1/5 text-center">{{ subject.subject_semester }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_code }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_name }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_grade }}</p>
+      <p class="basis-1/5 text-center">{{ subject.subject_result }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["subjects"]);
+</script>
--- client/src/views/my-info/ExamResultsView.vue
+++ client/src/views/my-info/ExamResultsView.vue
@@ -1,5 +1,66 @@
 <template>
-  <div id="exam-results-view">
-    <p>Exam Results View</p>
+  <div id="exam-results-view" class="py-8 px-16">
+    <ExamResultsCard :subjects="examResult" />
+    <div class="flex flex-row mx-4 my-8 justify-between">
+      <p class="text-xl font-medium">GPA : 8.80</p>
+      <p class="text-xl font-medium">Result Published on 29-09-2022</p>
+    </div>
+    <div>
+      <p class="text-xl mx-4">
+        For Students admitted from 2021 onwards: WH - With Held, U -
+        Re-appearance, SA - Shortage of Attendance <br />For others : WH - With
+        Held, RA* - Absent, RA - Re-appearance<br /><br />
+        ** WH - Please Contact Admin <br /><br />Disclaimer: The result
+        published at www.rajalakshmi.in is PROVISIONAL only. We are not
+        responsible for any inadvertent error that may have crept in the data /
+        results being published on the Internet. This is being published on the
+        Internet just for immediate information to the examinees. The final mark
+        sheets issued by the College should only be treated authentic and final
+        in this regard.
+      </p>
+    </div>
   </div>
 </template>
+
+<script setup>
+import ExamResultsCard from "../../components/my-info/ExamResultsCard.vue";
+import { ref } from "vue";
+
+const examResult = ref([
+  {
+    subject_semester: "4",
+    subject_code: "CS19441",
+    subject_name: "Operating Systems",
+    subject_grade: "A+",
+    subject_result: "Pass",
+  },
+  {
+    subject_semester: "4",
+    subject_code: "CS19442",
+    subject_name: "Software Engineering",
+    subject_grade: "A+",
+    subject_result: "Pass",
+  },
+  {
+    subject_semester: "4",
+    subject_code: "CS19443",
+    subject_name: "Database Management Systems",
+    subject_grade: "A+",
+    subject_result: "Pass",
+  },
+  {
+    subject_semester: "4",
+    subject_code: "CS19444",
+    subject_name: "Probability and Statistics",
+    subject_grade: "A+",
+    subject_result: "Pass",
+  },
+  {
+    subject_semester: "4",
+    subject_code: "CS19445",
+    subject_name: "Soft Skills",
+    subject_grade: "A+",
+    subject_result: "Pass",
+  },
+]);
+</script>
