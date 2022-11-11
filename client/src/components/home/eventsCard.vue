ccf1ade6eb1b4fd166a303c071c5059c41950c07
manosanjay-g <manosanjay.web@gmail.com>
2022-11-11 20:17:10 +0530

    finished HomeView UI

---
+++ client/src/components/home/eventsCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    id="events-card"
+    class="min-w-auto max-w-lg m-4 max-h-90 bg-neutral-900 rounded-md text-center py-2"
+  >
+    <p class="px-2 py-2 text-2xl font-medium">{{ props.card_name }}</p>
+    <hr />
+    <div
+      v-for="event in events"
+      :key="event.event_name"
+      class="m-4 flex flex-row rounded-md bg-neutral-800 items-center px-4 py-6"
+    >
+      <div class="flex flex-col basis-1/4">
+        <p>{{ event.event_date }}</p>
+        <p>{{ event.event_time }}</p>
+      </div>
+      <p class="basis-3/4 text-left">{{ event.event_name }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["card_name", "events"]);
+</script>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div id="home-view">
-    <p>Home View</p>
+  <div id="home-view" class="py-8 px-16 grid grid-cols-2">
+    <EventsCard card_name="GENERAL EVENTS" :events="generalEvents" />
+    <EventsCard card_name="PLACEMENT EVENTS" :events="generalEvents" />
+    <EventsCard card_name="TRAINING EVENTS" :events="generalEvents" />
+    <EventsCard card_name="RESEARCH PROJECTS" :events="generalEvents" />
   </div>
 </template>
+<script setup>
+import EventsCard from "../components/home/eventsCard.vue";
+import { ref } from "vue";
+
+const generalEvents = ref([
+  {
+    event_name: "Guest Lecture on AI",
+    event_date: "NOV 8",
+    event_time: "12:30 PM",
+  },
+  {
+    event_name: "Atrium Meet",
+    event_date: "NOV 8",
+    event_time: "2:00 PM",
+  },
+]);
+</script>
